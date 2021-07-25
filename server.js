013672a6121832b78f0e6351740a59d012dc9ad1
hariywill <814968538@qq.com>
2021-07-25 11:17:21 +1000

    Init

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const dotnev = require('dotenv')
+const colors = require('colors')
+const morgan = require('morgan')
+
+dotnev.config({ path: '/config/config.env' })
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
